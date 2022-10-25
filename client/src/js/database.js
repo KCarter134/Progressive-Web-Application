efc224cc848951b0b26d7986f79d7b069465bdfd
Kyle Carter <kcarter134@yahoo.com>
2022-10-25 16:31:33 -0400

    update webpack.config, install.js, database.js, && package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Update jate database content');
+  //connect to database
+  const jateDatabase = await openDB('jate', 1);
+  //specify database
+  const jateRead = jateDb.transaction('jate', 'readwrite');
+  //object store 
+  const objectStore = jateRead.objectStore('jate');
+  //add all content stored in DB
+  const req = objStore.put({id: id, value: value});
+  //add content
+  const res = await req;
+  console.log('Data request successful', res);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async (value) => {
+  console.error('Get jate database content');
+  //connect to database
+  const jateDatabase = await openDB('jate', 1);
+  //specify database
+  const jateRead = jateDb.transaction('jate', 'readwrite');
+  //object store 
+  const objectStore = jateRead.objectStore('jate');
+  //require all content stored in DB
+  const req = objStore.getAll();
+  //fetch content
+  const res = await req;
+  console.log('Data request successful', res);
+} 
 initdb();
